56727a0c745972c83e95521de0928125ff5f4685
SCsupercraft <alisonandant2020@gmail.com>
2025-02-24 16:00:07 +0000

    Upgrade from RR 7.1.5 to 7.2.0

--- scripts/.build/extension-data/code/scsupercraft's_extension_gallery/SCsupercraft/placeholder.js
+++
@@ -1,7 +0,0 @@
-// Name: Placeholder
-// ID: placeholder
-// Description: Uhh, a placeholder?
-// By: SCsupercraft <https://github.com/SCsupercraft>
-// License: MIT
-// Badge: Placeholder - This is a tooltip
-// Badge: Badge 2
