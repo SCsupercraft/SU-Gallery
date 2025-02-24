566d5515c8a414e76dda85f8d88295b443c5d837
SCsupercraft <alisonandant2020@gmail.com>
2025-02-24 17:17:58 +0000

    Update installDeps.mjs again!

--- scripts/installDeps.mjs
+++ scripts/installDeps.mjs
@@ -2,14 +2,14 @@ import { spawn } from 'node:child_process';
 
 async function spawnProcess(command) {
 	return new Promise((resolve) => {
-		const process = spawn(command, [], {
+		const newProcess = spawn(command, [], {
 			shell: true,
 			cwd: process.cwd(),
 			env: process.env,
 			stdio: 'inherit',
 			encoding: 'utf-8',
 		});
-		process.once('exit', resolve);
+		newProcess.once('exit', resolve);
 	});
 }
 
