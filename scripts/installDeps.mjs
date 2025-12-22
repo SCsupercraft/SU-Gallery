45b33f226f66bb33bcd9235b9c0781eb6e7ccaee
SCsupercraft <103709272+SCsupercraft@users.noreply.github.com>
2025-12-22 16:51:10 +0000

    Fix workflow

--- scripts/installDeps.mjs
+++ scripts/installDeps.mjs
@@ -1,9 +1,9 @@
 import { spawn } from 'node:child_process';
 import path from 'node:path';
+import { BuildHelper } from './helper.mjs';
 import fs from 'node:fs/promises';
-import { BuildHelper } from './helper.js';
 
-async function spawnProcess(command: string) {
+async function spawnProcess(command) {
 	return new Promise((resolve) => {
 		const newProcess = spawn(command, [], {
 			shell: true,
@@ -16,7 +16,6 @@ async function spawnProcess(command: string) {
 }
 
 const lock = path.resolve(process.cwd(), 'package-lock.json');
-
 if (await BuildHelper.exists(lock)) await fs.rm(lock);
 
 await spawnProcess('npm i --force');
